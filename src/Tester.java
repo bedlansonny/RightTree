346c93c911ae621c4b02547f3d94142aa6366d8f
bedlansonny <bedlansonny@gmail.com>
2018-01-31 15:54:54 -0600

    marked unfilled trees false instead of filling

--- src/Tester.java
+++ src/Tester.java
@@ -15,16 +15,17 @@ public class Tester
             int[] nums = new int[chars.length];
             for(int i = 0; i < chars.length; i++)
                 nums[i] = (int)chars[i]-48;
-            int[] filledArr = new int[(int)Math.pow(2, (int)Math.ceil(Math.log(nums.length+1)/Math.log(2)))-1];
-            System.arraycopy(nums, 0, filledArr, 0, nums.length);
-
-            System.out.println("Tree " + testCaseInd + (isRight(filledArr) ? " is " : " is not ") + "a right-tree.");
+            
+            System.out.println("Tree " + testCaseInd + (isRight(nums) ? " is " : " is not ") + "a right-tree.");
 
         }
     }
 
     public static boolean isRight(int[] nums)
     {
+        if(nums.length != (int)Math.pow(2, (int)Math.ceil(Math.log(nums.length+1)/Math.log(2)))-1)
+            return false;
+
         for(int i = nums.length-2; i > 0; i-=2)
         {
             if(nums[i] > nums[i+1])
